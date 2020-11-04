b8b04ad4a0b7cc40de20004bee2e51bdbeac6bf1
aleja1303 <73846046+aleja1303@users.noreply.github.com>
2020-11-04 07:34:30 -0600

    Update tareaaaaa.py

--- tareaaaaa.py
+++ tareaaaaa.py
@@ -3,81 +3,50 @@ import pandas as pd
 import pickle
 import matplotlib.pyplot as plt
 
-#Estabelcemos el data frame para la fecha
-print("\nPrograma de perros con Pandas :D\n")
 fronteras = pd.read_csv('frontera.csv')
-fronteras["Date"] = pd.to_datetime(fronteras["Date"], format="%m/%d/%Y")
-es_2019 = fronteras["Date"].dt.strftime("%Y") == "2019"
-#-------------------------------------------------------------------
+fecha = fronteras["Date"].isin(["1/1/2019 00:00", "2/1/2019 00:00", "3/1/2019 00:00", "4/1/2019 00:00","5/1/2019 00:00","6/1/2019 00:00","7/1/2019 00:00","8/1/2019 00:00","9/1/2019 00:00","10/1/2019 00:00","11/1/2019 00:00","12/1/2019 00:00"])
 
-#Establecemos el data frame de que estamos en la frontera de Canada
 es_Canada = fronteras["Border"] == "US-Canada Border"
-frontCanada = fronteras[es_Canada & es_2019]
-#------------------------------------------------------------------
+frontCanada = fronteras[es_Canada & fecha]
 
-#Creamos el data frame con ubicarnos en canada y sea el año 2019
 DataFrameCanada = pd.DataFrame(frontCanada)
 cols_to_subset = ["Port Name", "State", "Date", "Measure", "Value"]
 filtroFinal = DataFrameCanada[cols_to_subset]
 DataFrameFiltro = pd.DataFrame(filtroFinal)
-#print(filtroFinal)
-#--------------------------------------------------------------------
 
-#Creamos las condiciones de PVP y PV
+
+
+print(filtroFinal)
+
 es_PerVehPas = fronteras["Measure"] == "Personal Vehicle Passengers"
 es_PerVeh = fronteras["Measure"] == "Personal Vehicles"
-#--------------------------------------------------------------------
 
-#Creamos las condiciones de PD
 es_PD = fronteras["Measure"] == "Pedestrians"
 
-
-#---------------------------------------------------------------------
-
-
-#Creación de la ubicación de los estados
 es_AK = fronteras["State"] == "AK"
 
 
-
-#--------------------------------------------------------------------
-
-#Creamos el DataFrame de AKPVP, AKPV Para el ESTADO DE AK
-frameDePerAKPV = fronteras[es_2019 & es_Canada & es_PerVeh & es_AK]
+frameDePerAKPV = fronteras[fecha & es_Canada & es_PerVeh & es_AK]
 DataFrameAKPV = pd.DataFrame(frameDePerAKPV)
-#print(frameDePerAKPV)
-frameDePerAKPVP = fronteras[es_2019 & es_Canada & es_PerVehPas & es_AK]
+
+print(frameDePerAKPV)
+
+frameDePerAKPVP = fronteras[fecha & es_Canada & es_PerVehPas & es_AK]
 DataFrameAKPVP = pd.DataFrame(frameDePerAKPVP)
-#print(frameDePerAKPVP)
+
+print(frameDePerAKPVP)
 
 
-
-
-
-
-#-------------------------------------------------------------------------
-
-#Cremos el data frame de Pedestrians
-frameDeAKPD = fronteras[es_2019 & es_Canada & es_PD  & es_AK]
+frameDeAKPD = fronteras[fecha & es_Canada & es_PD  & es_AK]
 DataFrameAKPD = pd.DataFrame(frameDeAKPD)
-#print(DataFrameAKPD)
 
-
-
-
-#--------------------------------------------------------------------------
-
+print(DataFrameAKPD)
 
 #Gráfica de lineas por estado sin detalles de puertas, que muestra el movimiento de Personal Vehicles
 #xAKPD=DataFrameAKPD['Date']
 #yAKPD=DataFrameAKPD['Value']
 #plt.plot(xAKPD,yAKPD)
 #plt.show()
-
-
-
-
-#-------------------------------------------------------------------------------------------------------
 
 #Hacer Gráfica de puntos que muestre el comportamiento de "Pedestrians". Que sea una gráfica
 #por cada estado y con detalle de puertas en la horizontal
@@ -87,11 +56,6 @@ DataFrameAKPD = pd.DataFrame(frameDeAKPD)
 #plt.show()
 
 
-
-#--------------------------------------------------------------------------------------------------------
-
-
-#Creamos  los data frames con la condición de PV y PVP Por estado
 akPV= DataFrameAKPV[DataFrameAKPV['Measure']=='Personal Vehicles']
 akPVP= DataFrameAKPVP[DataFrameAKPVP['Measure']=='Personal Vehicle Passengers']
 dfakPV= pd.DataFrame(akPV)
@@ -101,46 +65,33 @@ sumaAKPVP = dfakPVP.groupby('Port Name')['Value'].sum()
 dfsumaAKPV= pd.DataFrame(sumaAKPV)
 dfsumaAKPVP= pd.DataFrame(sumaAKPVP)
 
+print(dfsumaAKPV)
+print(dfsumaAKPVP)
 
-
-
-
-
-#print(dfsumaAKPV)
-#print(dfsumaAKPVP)
-#---------------------------------------------------------------------------------------
-
-
-#Unimos PV y PVP POR CADA ESTADO dentro de un mismo data frame
 JuntosAK= pd.merge(dfsumaAKPV,dfsumaAKPVP,on='Port Name')
 dfJuntosAK=pd.DataFrame(JuntosAK)
 
-
-
-#print(dfJuntosAK)
-#-----------------------------------------------------------------------------------------
-
+print(dfJuntosAK)
 
 #Graficamos por estado las dos barras en una gráfica
-#JuntosAKx = dfJuntosAK[['Value_x','Value_y']]
-#JuntosAKx.plot.bar()
-#plt.show()
-
-
-#-----------------------------------------------------------------------------------------
-
-
-#Histograma de Buss pasengers para todos los estados
+JuntosAKx = dfJuntosAK[['Value_x','Value_y']]
+JuntosAKx.plot.bar()
+plt.show()
 
 es_BP = fronteras["Measure"] == "Bus Passengers"
-frameDeBP = fronteras[es_2019 & es_Canada & es_BP ]
+frameDeBP = fronteras[fecha & es_Canada & es_BP ]
 DataFrameBP = pd.DataFrame(frameDeBP)
-#print(DataFrameBP)
+print(DataFrameBP)
 
-#xBP=DataFrameBP['Value']
-#yBP=DataFrameBP[]
-#plt.hist(xBP)
-#plt.show()
 
-#DataFrameBP['Value'].plot.hist()
-#plt.show()
+xBP=DataFrameBP['Value']
+yBP=DataFrameBP
+plt.hist(xBP)
+plt.show()
+
+DataFrameBP['Value'].plot.hist()
+plt.show()
+
+
+
+
